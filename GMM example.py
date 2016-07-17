ce7ed2cad1b3947d11c1ead056fa2a26c951672b
Phil McDowall <philip.mcdowall@stonybrook.edu>
2016-07-17 17:16:28 -0400

    Updated to create polygons from raster image

--- GMM example.py
+++ GMM example.py
@@ -1,5 +1,7 @@
 import cv2
 from sklearn import mixture
+import numpy as np
+import matplotlib.pyplot as plt
 
 filePath = "C:\\Users\\Phil\\Desktop\\Sea Ice Photo for XSEDE16\\Examples\\072610_00104.jpg"
 #load image
@@ -16,3 +18,11 @@ cov = gmix.covars_
 #predict back to image
 labeled_img = gmix.predict(img.reshape((img.shape[0]*img.shape[1],3))).reshape(img.shape[0],img.shape[1])
 plt.imshow(labeled_img)
+
+
+#Find contours and plot
+contours = cv2.findContours(np.array(labeled_img,dtype="int32"),cv2.RETR_FLOODFILL,cv2.CHAIN_APPROX_SIMPLE)
+imgCont = img.copy()
+cv2.drawContours(imgCont, contours[0], -1, (0,255,0), 3)
+plt.imshow(imgCont)
+
