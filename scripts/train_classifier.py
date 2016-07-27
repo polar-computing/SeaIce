02ae458ecb0e7231a799a756c71a1551df89c2a6
Phil McDowall <philip.mcdowall@stonybrook.edu>
2016-07-27 13:14:35 -0400

    Fix of interactive selection, code formatting and create test script

--- scripts/train_classifier.py
+++ scripts/train_classifier.py
@@ -10,10 +10,11 @@ parser = argparse.ArgumentParser(description="Quicksift + random forest image cl
 parser.add_argument("-i", "--image", help="Image path")
 args = parser.parse_args()
 
+filePath = args.image
 
 # Quickshift parameters
-qs_kernel_size = 3
-qs_max_dist = 6
+qs_kernel_size = 7
+qs_max_dist = 5
 qs_ratio = 0.5
 
 # Training Samples parameters
@@ -24,22 +25,24 @@ RF_n_estimators = 100
 
 # Load image
 #TODO should use getImage
-img = misc.imread(filePath)
+img = misc.imread(filePath)[:,:,:3]
 print "Image loaded. ", img.shape[0], " x ", img.shape[1]
 fig, ax = plt.subplots()
 ax.imshow(img)
-plt.show(block=True)
+plt.show()
+
 print "Segmenting Image"
 # Quickshift segment image
 segments = quickshift(img, kernel_size=qs_kernel_size, max_dist=qs_max_dist, ratio=qs_ratio)
 print("Quickshift number of segments: %d" % len(np.unique(segments)))
 
 # Extract feature properties
-# Mean RGB intensities
-features = extractSegmentProperties(segments,img)
+features = extractSegmentPropertiesRGB(segments,img)
 
-labels = trainingSamplesMouse(img,segments)
-print labels
+#Interactively label image
+labels = trainingSamplesMouse(img,segments,maxClasses=5)
+
+#Create trainingset
 trainingSet, trainingSetClasses = createTrainingSet(labels,features)
 
 # Create classifier
@@ -54,7 +57,7 @@ output = forest.predict(features)
 # Save forest classifier for future use
 pickle.dump(forest, open("fittedforest.p", "wb"))
 
-#validate classifier
+#visual validation of classifier
 result_array = reclassify(segments, output)
 
 plt.imshow(result_array)
